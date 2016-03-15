fbf43b51e8e9b98a901faa3107d4cf488a2946eb
Wendy Wang <wangwenusa11@gmail.com>
2016-03-15 15:43:35 -0700

    Removed missing data

--- rscript.r
+++ rscript.r
@@ -3,3 +3,9 @@ Y <- read.csv("Downloads/S&Praw.csv", header=T, skip=35)
 X
 X <- X[complete.cases(X),]
 
+SPdata <- read.csv("~/timeseries443/SPdata.txt")
+View(SPdata)
+sp.ts <- ts(SPdata$VALUE, frequency = 5)
+ts.plot(sp.ts)
+
+
