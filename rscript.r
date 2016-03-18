691765b8f4f36648acfa88aa785aa7f55b596c8f
ceciliarong <vivirong@icloud.com>
2016-03-17 18:49:10 -0700

    Update rscript.r

--- rscript.r
+++ rscript.r
@@ -3,16 +3,32 @@ Y <- read.csv("Downloads/S&Praw.csv", header=T, skip=35)
 X
 X <- X[complete.cases(X),]
 
-SPdata <- read.csv("~/timeseries443/SPdata.txt")
+SPdata <- read.csv("~/timeseries443/spdata.csv")
 View(SPdata)
-sp.ts <- ts(SPdata$VALUE, frequency = 5, include.mean=TRUE)
+sp.ts <- ts(rev(SPdata$Adj.Close))
 ts.plot(sp.ts)
-
-#Take the first difference
-plot(diff(sp.ts))
-acf(diff(sp.ts))
-pacf(diff(sp.ts))
+acf(sp.ts)
+pacf(sp.ts)
 
 install.packages('astsa')
 library(astsa)
 spec.pgram(sp.ts, log="no")
+
+#Take the first difference
+sp.ts.diff < - diff(sp.ts)
+plot(sp.ts.diff)
+acf(sp.ts.diff)
+pacf(sp.ts.diff)
+spec.pgram(sp.ts.diff, log="no")
+
+#Take the forward ratio
+install.packages('FSA')
+library(FSA)
+sp.ts.ratio <- lagratio(sp.ts, lag = 1L, recursion = 1L, direction = "forward")
+
+plot(sp.ts.ratio)
+acf(sp.ts.ratio)
+pacf(sp.ts.ratio)
+spec.pgram(sp.ts.ratio, log="no")
+
+
